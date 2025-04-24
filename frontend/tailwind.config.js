b190cf00c104046c718c5104c2f579a8532a4e4c
Abhishekk-sj <oneplatformweb@gmail.com>
2025-04-24 23:37:39 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,12 @@
-
+// frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan React components
+    "./public/index.html",       // Scan the main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
