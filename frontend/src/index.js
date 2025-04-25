4b9c43b15208efe716f035aa032d69ff952f0d04
Abhishekk-sj <oneplatformweb@gmail.com>
2025-04-25 23:21:13 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,20 @@
 // frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css'; // We will put our main CSS/Tailwind here
-import App from './App';
-// reportWebVitals is optional, you can remove it
-// import reportWebVitals from './reportWebVitals';
+import './styles/index.css'; // Import your Tailwind CSS file
+import App from './App'; // Import your main App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Find the root element in index.html and create a React root
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+// Render the main App component into the root
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: You can remove the web-vitals import and call if you removed the dependency
+// import reportWebVitals from './reportWebVitals';
 // reportWebVitals();
